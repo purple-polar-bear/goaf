a2b61cacc24c7b71cfde5e687d0c97be03ca40cf
Wouter <wouter.visscher@gmail.com>
2021-12-01 16:05:56 +0100

    tabs and spaces stuff

--- package/features/feature_service.go
+++ package/features/feature_service.go
@@ -3,7 +3,7 @@ package features
 import "net/http"
 
 type FeatureService interface {
-	Collections() []Collection
-	Collection(string) Collection
-	Features(*http.Request, *FeaturesParams) Features
+  Collections() []Collection
+  Collection(string) Collection
+  Features(*http.Request, *FeaturesParams) Features
 }
